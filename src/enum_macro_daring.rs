3d1636481bfc7623a4104fe187ee031f61ac011e
Xie YongSheng <xietbro@163.com>
2024-09-16 22:33:11 +0800

    fix:添加use darling注释

--- src/enum_macro_daring.rs
+++ src/enum_macro_daring.rs
@@ -6,6 +6,7 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::DeriveInput;
 
+// 注册FromDeriveInput宏，将DeriveInput转换为EnumFromDarling做支持，获取input里的ident、generics、data
 #[derive(Debug, FromDeriveInput)]
 struct EnumFromDarling {
     ident: syn::Ident,
@@ -13,21 +14,25 @@ struct EnumFromDarling {
     data: Data<EnumVariants, ()>,
 }
 
+// 获取单个枚举变体的信息
 #[derive(Debug, FromVariant)]
 struct EnumVariants {
     ident: syn::Ident,
     fields: Fields<EnumVariantFields>,
 }
 
+// 获取枚举字段的类型信息
 #[derive(Debug, FromField)]
 struct EnumVariantFields {
     ty: syn::Type,
 }
 
 pub(crate) fn process_enum_from_darling(input: DeriveInput) -> TokenStream {
+    // let 匹配 枚举类型，如果匹配失败，则 panic
     let EnumFromDarling {
         ident,
         generics,
+        // 如果不是枚举类型，则 panic
         data: Data::Enum(data),
     } = EnumFromDarling::from_derive_input(&input).expect("can not parse input")
     else {
@@ -38,6 +43,7 @@ pub(crate) fn process_enum_from_darling(input: DeriveInput) -> TokenStream {
         let var = &variant.ident;
         let style = &variant.fields.style;
         match style {
+            // style可以识别为Unit、Named、Unnamed三种类型
             Style::Tuple if variant.fields.len() == 1 => {
                 let field = variant.fields.iter().next().expect("should have 1 field");
                 let ty = &field.ty;
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use enum_macro::process_enum_from;
 use enum_macro_daring::process_enum_from_darling;
 // proc-macro
 use proc_macro::TokenStream;
-// 注册EnumFrom过程宏，自动为 变体 生成From<T> trait实现
+// 注册EnumFrom过程宏的实现，自动为 变体 生成From<T> trait实现
 #[proc_macro_derive(EnumFrom)]
 pub fn derive_enum_from(input: TokenStream) -> TokenStream {
     // input是proc-macro的输入（代表要处理的代码），返回新生成的代码
@@ -15,6 +15,7 @@ pub fn derive_enum_from(input: TokenStream) -> TokenStream {
     process_enum_from(input).into()
 }
 
+// 注册EnumFromDarling过程宏的实现
 #[proc_macro_derive(EnumFromDarling)]
 pub fn derive_enum_from_darling(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
